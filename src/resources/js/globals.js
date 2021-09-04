e10e6530891b6908b33437b6ab120e7b6409603c
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-09-03 22:10:38 -0300

    subindo alterações para executar o projeto no git pages

--- src/resources/js/globals.js
+++ src/resources/js/globals.js
@@ -1,5 +1,5 @@
 const globals = {
-  auth: "Key API HERE'",
+  auth: "563492ad6f91700001000001ad8ed003f71f45448e4ca49917867178",
   columns: document.querySelectorAll("article section"),
   article: document.querySelector("article"),
   form: document.querySelector("form"),
